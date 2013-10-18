abc50dec182e9e3e62c278225b5f287e51c405e7
Achal Aggarwal <aggarwal.achal@gmail.com>
2013-10-18 16:35:02 +0530

    step4 complete

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 App = Ember.Application.create();
 
+var counter = 0;
+
 var Bookmark = Ember.Object.extend({
   to_link: function() {
     return "<a href='" + this.get('url') + "'>" + this.get('name') + "</a>";
@@ -10,6 +12,10 @@ var Bookmark = Ember.Object.extend({
 , link: function() {
     return this.to_link()
   }.property("name", "url")
+, updateCounter: function() {
+    counter += 1;
+    console.info(counter);
+  }.observes("name")
 });
 
 var bookmark = Bookmark.create({'name': 'airwoot', 'url': 'http://airwoot.com'});
