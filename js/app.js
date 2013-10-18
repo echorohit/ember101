f4a39aa8ce8295f781cf17fe45f5738ae1728d0a
Achal Aggarwal <aggarwal.achal@gmail.com>
2013-10-18 16:16:40 +0530

    step2 complete

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 App = Ember.Application.create();
+
+var Bookmark = Ember.Object.extend({
+  to_link: function() {
+    return "<a href='" + this.get('url') + "'>" + this.get('name') + "</a>";
+  }
+});
+
+Bookmark.reopen({
+  formatted: function() {
+    return this.get('name') + " - " + this.get("url");
+  }
+});
+
+var bookmark = Bookmark.create({'name': 'airwoot', 'url': 'http://airwoot.com'});
+
+
