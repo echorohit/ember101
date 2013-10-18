eb56b54b8c87be0b2d8d3c872ddbe69fca97f8c7
Achal Aggarwal <aggarwal.achal@gmail.com>
2013-10-18 18:55:06 +0530

    added single bookmark display page. step8 complete

--- js/router.js
+++ js/router.js
@@ -4,19 +4,16 @@ App.Router.map(function() {
   });
 
   this.route('contact');
-
   this.resource('bookmarks');
+  this.resource('bookmark', {path: '/bookmarks/:bookmark_id'});
 });
 
 
-var bookmark1 = App.Bookmark.create({'name': 'airwoot', 'url': 'http://airwoot.com'});
-var bookmark2 = App.Bookmark.create({'name': 'google', 'url': 'http://google.com'});
-
+var bookmark1 = App.Bookmark.create({id: 1, 'name': 'airwoot', 'url': 'http://airwoot.com'});
+var bookmark2 = App.Bookmark.create({id: 2, 'name': 'google', 'url': 'http://google.com'});
 
 App.BookmarksRoute = Ember.Route.extend({
   model: function() {
     return [bookmark1, bookmark2];
   }
 });
-
-
