3aabfc7a9e2ed4a8aa30429f42b5e8db8f682ae6
Achal Aggarwal <aggarwal.achal@gmail.com>
2013-10-18 16:27:43 +0530

    step3 complete

--- js/app.js
+++ js/app.js
@@ -4,12 +4,12 @@ var Bookmark = Ember.Object.extend({
   to_link: function() {
     return "<a href='" + this.get('url') + "'>" + this.get('name') + "</a>";
   }
-});
-
-Bookmark.reopen({
-  formatted: function() {
+, formatted: function() {
     return this.get('name') + " - " + this.get("url");
   }
+, link: function() {
+    return this.to_link()
+  }.property("name", "url")
 });
 
 var bookmark = Bookmark.create({'name': 'airwoot', 'url': 'http://airwoot.com'});
